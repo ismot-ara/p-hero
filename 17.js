42937f97369975ea8f23db13936eb8726939d6cc
Ismot Ara Srity <ismotara107@gmail.com>
2023-02-06 07:48:31 +0600

    first commit

---
+++ 17.js
@@ -0,0 +1,26 @@
+//17-0
+//salad.pop();
+//pizza.slice(2,4);
+//substring();
+console.log('Bismillah');
+//+=
+//string to number
+var price1 = '50';
+var gpa = '50.56';
+price1 = parseInt(price1);
+console.log(price1);
+gpa = parseFloat(gpa);
+console.log(gpa);
+
+console.log(typeof gpa);
+
+//17-9 (6.13) 0.1+ 0.2 (problem explain)
+var num1 = 0.1;
+var num2 = 0.2;
+var sum = num1 + num2;
+sum = sum.toFixed(2);
+console.log(sum);
+sum = parseFloat(sum);
+console.log(sum);
+//17-10 what is javascript
+// letter, digits , underscore,dollar(variable)
