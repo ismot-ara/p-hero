78d9e82d546cfccae85bee4b8b6d1f914661fca3
Ismot Ara Srity <ismotara107@gmail.com>
2023-02-24 21:09:41 +0600

    20.3 complete

---
+++ 20.js
@@ -0,0 +1,14 @@
+var color = ['white', 'red', 'green', 'blue', 'yellow'];
+var index = color.indexOf('red');
+console.log(index);
+
+var slice_exam = color.slice(1,4);
+console.log(slice_exam); 
+
+var name = "she has a pen";
+var a = name.includes('p');
+console.log(a);
+
+var text= "ismot ara";
+var b = text.substring(1, 7);
+console.log(b);
